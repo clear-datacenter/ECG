aec170359652b1af084cdc20a39b5dade6429384
Tao-Quixote <web.taox@gmail.com>
2016-07-12 11:23:41 +0800

    'conflict'

--- js/test.js
+++ js/test.js
@@ -3,8 +3,17 @@
  * CreateTime:  16/6/22 16:20
  */
 
-test();
-
-var test = function () {
-    console.log('xx');
-}
+var obj = {
+    value: 100,
+    get getVal(){
+        return this.value;
+    },
+    set setVal(val) {
+        this.value = val;
+    }
+};
+Object.defineProperty(obj,'value', {
+    enumerable: false,
+    configurable: true,
+    writable: false
+});
